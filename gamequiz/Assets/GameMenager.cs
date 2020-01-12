46c22c8790a5c6ade96fcafc1e4b777f60ed4e8e
Kamil <kamild160@gmail.com>
2020-01-12 14:44:26 +0100

    Layaut - Quiz
    Layout of quiz game for PC

---
+++ gamequiz/Assets/GameMenager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenager : MonoBehaviour
+{
+    public Question[] questions;
+
+    private void Start()
+    {
+       
+    }
+
+}

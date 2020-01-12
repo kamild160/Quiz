380d24acc8c07e36e90343046d2bb63534f9eeac
Kamil <kamild160@gmail.com>
2020-01-12 15:06:30 +0100

    Scripts beggining

--- gamequiz/Assets/GameMenager.cs
+++ gamequiz/Assets/GameMenager.cs
@@ -1,14 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameMenager : MonoBehaviour
 {
     public Question[] questions;
+    private static List<Question> unAnswered;
+
+    private Question currentQuestion;
 
     private void Start()
     {
+        if (unAnswered == null || unAnswered.Count == 0) //Jeżeli jest puste lub odpwoeidzniano na wszystkie pytania
+        {
+            unAnswered = questions.ToList<Question>();
+
+
+        }
+
+        GetRandomQuestion();
+        Debug.Log(currentQuestion.question + " is " + currentQuestion.isTrue);
        
+    }
+
+    void GetRandomQuestion()//wybieramy randomowe pytanie z listy
+    {
+        int randomQuestionID = Random.Range(0, unAnswered.Count);
+        currentQuestion = unAnswered[randomQuestionID];
+
+        unAnswered.RemoveAt(randomQuestionID); //usuwamy to pytanie z listy 
+
     }
 
 }

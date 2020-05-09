5c6463fcef110d44568b8e3187bf7dfda79a030e
Kamil <kamild160@gmail.com>
2020-05-09 16:47:15 +0200

    Join room part 1
    Dodane ekran dołączenia do gry - początek implementacji
    
    Inne mniejsze poprawki

---
+++ gamequiz/Assets/Scripts/nazwagracza.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon;
+using Photon.Pun;
+
+public class nazwagracza : MonoBehaviour
+{
+    [SerializeField] private InputField nameInput = null;
+    [SerializeField] private Button contininueButton = null;
+
+    private const string Playernick = "PlayerName";
+
+   
+    private void Start()
+    {
+        SetUpInputField();
+    }
+
+    private void SetUpInputField()
+    {
+        if (!PlayerPrefs.HasKey(Playernick))
+        { return; }
+
+        string defaultName = PlayerPrefs.GetString(Playernick);
+        nameInput.text = defaultName;
+
+        SetPlayerName(defaultName);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        contininueButton.interactable = !string.IsNullOrEmpty(name);
+
+    }
+
+    public void SaveName()
+    {
+        string playerName = nameInput.text;
+
+        PhotonNetwork.NickName = playerName;
+        PlayerPrefs.SetString(Playernick, playerName);
+
+
+    }
+}
---
+++ gamequiz/Assets/Scripts/startgame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Concurrent;
+
+public class startgame : MonoBehaviour
+{
+   
+
+    public void LoadScene(string level)
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+        SceneManager.LoadScene(level);
+    }
+}
